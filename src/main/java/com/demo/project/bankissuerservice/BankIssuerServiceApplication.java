3bc1d1ae62d274e08ae6d7568a6c1d1cddc657e9
Vladyslav Shakhno <vladyslav.shakhno@cengage.com>
2024-09-20 15:55:05 +0300

    initial commit

---
+++ src/main/java/com/demo/project/bankissuerservice/BankIssuerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.project.bankissuerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIssuerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankIssuerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/project/bankissuerservice/BankIssuerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.project.bankissuerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankIssuerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
